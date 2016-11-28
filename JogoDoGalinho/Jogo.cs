c51910c9f67eaf42a84c987e108fac1b54187f22
Francisco <francisco.esteves.leal@gmail.com>
2016-11-28 01:51:04 +0000

    1st commit
    1st commit

---
+++ JogoDoGalinho/Jogo.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDoGalinho
+{
+    class Jogo
+    {
+
+            private int pontosA;
+            private int pontosB;
+            private bool primeiro;
+            private bool emJogo;
+            private int x;
+            private int y;
+            private char[,] quadro = new char[3, 3];
+            private char jogadorA = 'A';
+            private char jogadorB = 'B';
+
+
+            public Jogo()
+            {
+                x = 3;
+                y = 3;
+                pontosA = 0;
+                pontosB = 0;
+                emJogo = false;
+                quadro = new char[3, 3];
+            }
+
+            public void preencheQuadro()
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    for (int a = 0; a < y; a++)
+                    {
+                        quadro[i, a] = 'C';
+                    }
+                }
+
+            }
+
+            public void startGame()
+            {
+                emJogo = true;
+                primeiro = true;
+
+            }
+
+            public void ocupaEspaco(int x, int y)
+            {
+                if (primeiro == true)
+                {
+                    quadro[x, y] = jogadorA;
+                }
+                else
+                {
+                    quadro[x, y] = jogadorB;
+                }
+
+            }
+
+        public bool verificaOcupacao(int newX, int newY)
+        {
+            if(quadro[newX, newY] != 'C')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+            public void print()
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    for (int a = 0; a < y; a++)
+                    {
+                        Console.Write(quadro[i, a]);
+                    }
+                    Console.Write("\n");
+                }
+            }
+            
+
+        public char checkWin()
+        {
+            char currentPlayer;
+            if (primeiro == true)
+            {
+                currentPlayer = 'B';
+            }
+            else
+            {
+                currentPlayer = 'A';
+            }
+
+            //VERTICAL
+            if (quadro[0, 0] == quadro[0, 1] && quadro[0, 1] == quadro[0, 2] && quadro[0, 2] != 'C')
+            {
+                return currentPlayer;
+            }
+            else
+            {
+                if (quadro[1, 0] == quadro[1, 1] && quadro[1, 1] == quadro[1, 2] && quadro[1, 2] != 'C')
+                {
+                    return currentPlayer;
+                }
+                else
+                {
+                    if (quadro[2, 0] == quadro[2, 1] && quadro[2, 1] == quadro[2, 2] && quadro[2, 2] != 'C')
+                    {
+                        return currentPlayer;
+                    }
+                    else
+                    {
+                        //HORIZONTAL
+                        if (quadro[0, 0] == quadro[1, 0] && quadro[1, 0] == quadro[2, 0] && quadro[2, 0] != 'C')
+                        {
+                            return currentPlayer;
+                        }
+                        else
+                        {
+                            if (quadro[0, 1] == quadro[1, 1] && quadro[1, 1] == quadro[2, 1] && quadro[2, 1] != 'C')
+                            {
+                                return currentPlayer;
+                            }
+                            else
+                            {
+                                if (quadro[0, 2] == quadro[1, 2] && quadro[1, 2] == quadro[2, 2] && quadro[2, 2] != 'C')
+                                {
+                                    return currentPlayer;
+                                }
+                                else
+                                {
+                                    //OBLIQUAS
+                                    if (quadro[0, 0] == quadro[1, 1] && quadro[1, 1] == quadro[2, 2] && quadro[2, 2] != 'C')
+                                    {
+                                        return currentPlayer;
+                                    }
+                                    else
+                                    {
+                                        if (quadro[2, 0] == quadro[1, 1] && quadro[1, 1] == quadro[0, 2] && quadro[0, 2] != 'C')
+                                        {
+                                            return currentPlayer;
+                                        }
+                                        else
+                                        {
+                                            return 'C';
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public string marca()
+        {
+            if(primeiro == true)
+            {
+                primeiro = false;
+                return "X";
+            }
+            else
+            {
+                primeiro = true;
+                return "O";
+            }
+        }
+
+
+
+
+    }
+}
---
+++ JogoDoGalinho/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace JogoDoGalinho
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        Jogo teste;
+        public MainWindow()
+        {
+            InitializeComponent();
+            teste = new Jogo();
+            teste.preencheQuadro();
+            teste.startGame();
+            teste.print();
+        }
+
+        private void b_1_Click(object sender, RoutedEventArgs e)
+        {
+            if (teste.verificaOcupacao(0, 0) != true)
+            {
+                teste.ocupaEspaco(0, 0);
+                teste.print();
+                b_1.Content = teste.marca();
+                if (teste.checkWin() != 'C')
+                {
+                    MessageBox.Show("O jogador " + teste.checkWin() + " ganhou!");
+                }
+            }
+
+        }
+
+        private void b_2_Click(object sender, RoutedEventArgs e)
+        {
+            if (teste.verificaOcupacao(1, 0) != true)
+            {
+                teste.ocupaEspaco(1, 0);
+                teste.print();
+                b_2.Content = teste.marca();
+                if (teste.checkWin() != 'C')
+                {
+                    MessageBox.Show("O jogador " + teste.checkWin() + " ganhou!");
+                }
+            }
+        }
+
+        private void b_3_Click(object sender, RoutedEventArgs e)
+        {
+            if (teste.verificaOcupacao(2, 0) != true)
+            {
+                teste.ocupaEspaco(2, 0);
+                teste.print();
+                b_3.Content = teste.marca();
+                if (teste.checkWin() != 'C')
+                {
+                    MessageBox.Show("O jogador " + teste.checkWin() + " ganhou!");
+                }
+            }
+        }
+
+        private void b_4_Click(object sender, RoutedEventArgs e)
+        {
+            if (teste.verificaOcupacao(0, 1) != true)
+            {
+                teste.ocupaEspaco(0, 1);
+                teste.print();
+                b_4.Content = teste.marca();
+                if (teste.checkWin() != 'C')
+                {
+                    MessageBox.Show("O jogador " + teste.checkWin() + " ganhou!");
+                }
+            }
+        }
+
+        private void b_5_Click(object sender, RoutedEventArgs e)
+        {
+            if (teste.verificaOcupacao(1, 1) != true)
+            {
+                teste.ocupaEspaco(1, 1);
+                teste.print();
+                b_5.Content = teste.marca();
+                if (teste.checkWin() != 'C')
+                {
+                    MessageBox.Show("O jogador " + teste.checkWin() + " ganhou!");
+                }
+            }
+        }
+
+        private void b_6_Click(object sender, RoutedEventArgs e)
+        {
+            if (teste.verificaOcupacao(2, 1) != true)
+            {
+                teste.ocupaEspaco(2, 1);
+                teste.print();
+                b_6.Content = teste.marca();
+                if (teste.checkWin() != 'C')
+                {
+                    MessageBox.Show("O jogador " + teste.checkWin() + " ganhou!");
+                }
+            }
+        }
+
+        private void b_7_Click(object sender, RoutedEventArgs e)
+        {
+            if (teste.verificaOcupacao(0, 2) != true)
+            {
+                teste.ocupaEspaco(0, 2);
+                teste.print();
+                b_7.Content = teste.marca();
+                if (teste.checkWin() != 'C')
+                {
+                    MessageBox.Show("O jogador " + teste.checkWin() + " ganhou!");
+                }
+            }
+        }
+
+        private void b_8_Click(object sender, RoutedEventArgs e)
+        {
+            if (teste.verificaOcupacao(1, 2) != true)
+            {
+                teste.ocupaEspaco(1, 2);
+                teste.print();
+                b_8.Content = teste.marca();
+                if (teste.checkWin() != 'C')
+                {
+                    MessageBox.Show("O jogador " + teste.checkWin() + " ganhou!");
+                }
+            }
+        }
+
+        private void b_9_Click(object sender, RoutedEventArgs e)
+        {
+            if (teste.verificaOcupacao(2, 2) != true)
+            {
+                teste.ocupaEspaco(2, 2);
+                teste.print();
+                b_9.Content = teste.marca();
+                if (teste.checkWin() != 'C')
+                {
+                    MessageBox.Show("O jogador " + teste.checkWin() + " ganhou!");
+                }
+            }
+        }
+    }
+}
